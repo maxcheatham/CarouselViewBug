66378b51593022daa301970ce21511628b74e9ec
Max Cheatham <max.cheatham@bluedotsolutions.com>
2025-04-29 16:37:01 -0600

    remove count and counter

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,27 +5,12 @@ namespace CarouselViewBug
     public partial class MainPage : ContentPage
     {
         readonly MainViewModel _vm;
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
             BindingContext = new MainViewModel();
             _vm = (MainViewModel)BindingContext;
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            _vm.ModifyItemsSource();
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
 
         private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
